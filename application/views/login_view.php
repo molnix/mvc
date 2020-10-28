81a1521f86ce0e7fc700c384b190355e840ffab6
molnix <49219804+molnix@users.noreply.github.com>
2020-10-28 11:04:16 +0700

    Add files via upload

---
+++ application/views/login_view.php
@@ -0,0 +1,7 @@
+<form action="" method="post">
+<p>Авторизация</p>
+<p>Введите логин: <input type="text" name="login"></p>
+<p>Введите пароль: <input type="password" name="password"></p>
+<p><input type="submit"></p>
+</form>
+<?=$data['errors']?>
